52d509c43cd7a172a977e9826ddc841b2b4f4dfd
FlexingBaby <zudio970@gmail.com>
2024-05-16 23:25:31 +0530

    Update Ns_Followers.py

--- Ns_Followers.py
+++ Ns_Followers.py
@@ -11,9 +11,9 @@ class Ns_Followers:
         self.requests=requests
         self.__AES_NoPadding = AES_CBC_NoPadding()
 
-        self.__userid='...' #Enter UserID Here
-        self.__username='...' #Enter UserName Here
-        self._session='...' #Enter SessionID Here
+        self.__userid='66432060720' #Enter UserID Here
+        self.__username='leviskevin01' #Enter UserName Here
+        self._session='66432060720:w7JFWAY9gJN833:17:AYfh_xFfKpaxSeMZHeOt5FkQye8OPR4F0wDHHEzbaA' #Enter SessionID Here
 
         self.__Pk=self.__userid.encode()+b'*'+str(random.randint(111,999)).encode()
         self.__Ip=os.urandom(8).hex().encode()+b'*'+str(random.randint(111,999)).encode()
